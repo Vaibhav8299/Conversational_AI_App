e5b25394dfc4843df951d8598992ba2464b47018
Vaibhav8299 <154986384+Vaibhav8299@users.noreply.github.com>
2024-08-27 12:23:28 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="multilingual assistant",
+    name="multilingual_assistant",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["SpeechRecognition","gTTS","google-generativeai","python-dotenv","streamlit","requests","googlemaps","google-search-results","playsound"]
+    install_requires=[
+        "streamlit",
+        "requests",
+        "SpeechRecognition",
+        "google-generativeai",
+        "gTTS",
+        "playsound",
+        "langchain"
+    ],
 )
